bcaf631ec5c1c461597b7c309fe36fc157830896
bima bagaskhoro <bima.bagaskhoro@gmail.com>
2025-08-16 00:06:33 +0700

    integrate with?

--- miner/logic/job_handler.py
+++ miner/logic/job_handler.py
@@ -147,6 +147,8 @@ def _load_and_modify_config_diffusion(job: DiffusionJob) -> dict:
         config["train_data_dir"] = f"/dataset/images/{job.job_id}/img/"
         config["huggingface_token"] = cst.HUGGINGFACE_TOKEN
         config["huggingface_repo_id"] = f"{cst.HUGGINGFACE_USERNAME}/{job.expected_repo_name or str(uuid.uuid4())}"
+        from subimage.sdxl_config import _load_and_modify_config_diffusion_sdxl
+        _load_and_modify_config_diffusion_sdxl(job)
     elif job.model_type == ImageModelType.FLUX:
         with open(cst.CONFIG_TEMPLATE_PATH_DIFFUSION_FLUX, "r") as file:
             config = toml.load(file)
@@ -154,6 +156,8 @@ def _load_and_modify_config_diffusion(job: DiffusionJob) -> dict:
         config["train_data_dir"] = f"/dataset/images/{job.job_id}/img/"
         config["huggingface_token"] = cst.HUGGINGFACE_TOKEN
         config["huggingface_repo_id"] = f"{cst.HUGGINGFACE_USERNAME}/{job.expected_repo_name or str(uuid.uuid4())}"
+        from subimage.flux_config import _load_and_modify_config_diffusion_flux
+        _load_and_modify_config_diffusion_flux(job)
     else:
         logger.error(f"Unknown model type: {job.model_type}")
     return config
